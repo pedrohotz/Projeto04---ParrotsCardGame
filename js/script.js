866db0a2b3a2bb1e407c1d38786ee592575e8e1a
Pedro Hotz <pedrohotz2002@gmail.com>
2021-08-01 15:40:02 -0300

    Configuração de cartas Adicionada

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-let qtdCartas = 0;
+
+let primeira_carta;
+let segunda_carta;
+let gifsExibir = [];
 let gifs = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif","metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"];
 gifs.sort(comparador);
-let cartasMarcadas = 0;
-
+let cartasMarcadas = [];
+let cliques = 0;
 liberarJogo();
 inserirCartas();
 function liberarJogo(){
@@ -21,7 +24,6 @@ function comparador() {
 function inserirCartas(){
 
     let inserir = "";
-    let gifsExibir = [];
     const cartasDiv = document.querySelector(".cartas");
     for(let i =0; i < (qtdCartas/2); i++){
         gifsExibir.push(gifs[i]);
@@ -41,7 +43,65 @@ function inserirCartas(){
 }
 
 function virarCarta(carta){
+    cliques++;
+    if(primeira_carta !== undefined && segunda_carta !== undefined){
+        primeira_carta = undefined;
+        segunda_carta = undefined;
+        cartasMarcadas = [];
+    }
+    let gif = carta.querySelector(".gif");
+    let stringGif = gif.getAttribute('src');
+    cartasMarcadas.push(stringGif);
+    if(primeira_carta == undefined){
+        primeira_carta = carta;
+        primeira_carta.classList.add("is-flipped");
+    }
+    else {
+        segunda_carta = carta;
+        segunda_carta.classList.add("is-flipped");
+        if (cartasMarcadas.length === 2){
+            if(verificarAcerto() == true){
+                primeira_carta.classList.add("is-flipped");
+                segunda_carta.classList.add("is-flipped");
+                verificarVitoria();
+            }
+            else{
+                setTimeout(function(){
+                    primeira_carta.classList.remove("is-flipped");
+                    segunda_carta.classList.remove("is-flipped");
+                },1000);
+            }
+        }
+    }
 
-    carta.classList.add("is-flipped");
+console.log(cartasMarcadas);
 }
 
+
+function verificarVitoria(){
+    const cartas = document.querySelectorAll(".card");
+    let acertos = 0;
+    for(let i = 0;i < cartas.length;i++){
+        if(cartas[i].classList.contains("is-flipped")){
+            acertos++;
+        }
+    }
+   if (acertos == cartas.length){
+       setTimeout(function(){
+        alert("Você Venceu !!!!!!!!");
+       },1000);
+   }
+
+}
+
+
+function verificarAcerto(){
+    for(let i=0;i < cartasMarcadas.length;i++){
+        if(cartasMarcadas[i] === cartasMarcadas[i+1]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
